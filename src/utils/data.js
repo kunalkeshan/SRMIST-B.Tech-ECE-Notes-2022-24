a6bf7619d98d3c8e56387a7481c62cb911dce62f
kunalkeshan <kunalkeshan12@gmail.com>
2022-07-20 13:05:46 +0530

    documentation

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,6 +14,8 @@ import semester6 from '../data/semesters/semester6.json';
 import semester7 from '../data/semesters/semester7.json';
 import semester8 from '../data/semesters/semester8.json';
 
+// Get Read Notifications from Local Storage, parse it if it's there
+// If not there, then return an empty array.
 const readNotifications = localStorage.getItem('readNotifications') ? JSON.parse(localStorage.getItem('readNotifications')) : [];
 const coursesOfSems = {
     semester1,
@@ -26,10 +28,18 @@ const coursesOfSems = {
     semester8,
 }
 
+/**
+ * Data Utility Class
+ */
 class Data {
-    Notification() {
+    static get Notification() {
         const notificationUtils = {};
 
+        /**
+         * @description Compares read notifications and new notifications, 
+         * returns true if already read and false if not for *read* property.
+         * @returns {array} Array of All notifications
+         */
         notificationUtils.getAllNotification = async () => {
             try {
                 allNotifications.forEach((notification) => {
@@ -44,14 +54,19 @@ class Data {
             }
         }
 
-        notificationUtils.updateReadNotifications = async (notificationId) => {
+        /**
+         * @description Update notification as read using notification Id
+         * @param {string} notificationId notification Id to be added to read notifications
+         * @returns {string} 'Notification marked as read!'
+         */
+        notificationUtils.updateReadNotifications = async (notificationId = '') => {
             try {
                 const readNotification = allNotifications.find(notification => notification.id === notificationId);
                 if (!readNotification) throw new Error('Notification with give Id does not exist!')
                 readNotification.read = true;
                 readNotifications.push(readNotification);
                 localStorage.setItem('readNotifications', JSON.stringify(readNotifications));
-                return 'Notification marked as read!'
+                return 'Notification marked as read!';
             } catch (error) {
                 return Promise.reject(error);
             }
@@ -60,13 +75,22 @@ class Data {
         return notificationUtils;
     }
 
-    Semesters() {
+    static get Semesters() {
         const semestersUtility = {};
 
+        /**
+         * @description get all semester information as an array of objects
+         * @returns {array} Array of all Semester Information
+         */
         semestersUtility.getAllSemesters = async () => {
             return allSemesters;
         };
 
+        /**
+         * @description Get all courses of a particular semester using Sem Name
+         * @param {string} semName 
+         * @returns {array} Array of courses of the sem requested
+         */
         semestersUtility.getCoursesOfSemester = async (semName) => {
             return coursesOfSems[semName];
         };
