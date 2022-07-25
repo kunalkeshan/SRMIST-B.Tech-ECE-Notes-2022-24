138b8ea0afd4a2951c3fd5145ef7c3b42bf819a3
kunalkeshan <kunalkeshan12@gmail.com>
2022-07-25 09:50:11 +0530

    not found page updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+/**
+ * Not Found Page
+ */
+
+// Dependencies
+import React, { useMemo } from 'react';
+import { useNavigate } from 'react-router';
+import Lottie from 'react-lottie';
+import config from '../config';
+
+// MUI
+import { Box, styled, Button } from '@mui/material';
+
+// Custom 
+import NotFoundAnimationData from '../assets/lottie/404-not-found-page.json';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
+    const defaultOptions = useMemo(() => {
+        return {
+            loop: true,
+            autoplay: true,
+            animationData: NotFoundAnimationData,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        }
+    }, []);
+
+    const handleNavigate = () => {
+        navigate('/')
+    };
+
     return (
-        <div>NotFound</div>
+        <Container>
+            <Lottie
+                options={defaultOptions}
+                width='clamp(200px, 100%, 800px)'
+                height='auto'
+            />
+            <Content>
+                <GoBackButton
+                    variant='contained'
+                    onClick={handleNavigate}
+                >Go Back Home</GoBackButton>
+            </Content>
+        </Container>
     )
 }
 
-export default NotFound
+const Container = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+});
+
+const Content = styled(Box)({
+    padding: '1em',
+    margin: '1em',
+});
+
+const GoBackButton = styled(Button)({
+    backgroundColor: config.APP_COLORS.accent,
+    '&:hover': {
+        backgroundColor: config.APP_COLORS.accent,
+    }
+});
+
+export default NotFound;
