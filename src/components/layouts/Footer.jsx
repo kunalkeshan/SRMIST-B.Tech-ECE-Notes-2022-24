f693f2f5a3b87d1cd488064c531ec0c1a50e0dda
kunalkeshan <kunalkeshan12@gmail.com>
2022-07-25 09:41:55 +0530

    footer nav added

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,14 +3,52 @@
  */
 
 // Dependencies
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router';
 
 // MUI
 import { Box, styled, Typography, Link } from '@mui/material';
 
 const Footer = () => {
+    const navigate = useNavigate();
+
+    const links = [
+        {
+            name: 'Home',
+            nav: '/',
+        },
+        {
+            name: 'Contribute',
+            nav: '/contribute',
+        },
+        {
+            name: 'Contact',
+            nav: '/contact',
+        },
+        {
+            name: 'Support',
+            nav: '/support',
+        },
+        {
+            name: 'Repo',
+            link: 'https://github.com/kunalkeshan/SRMIST-B.Tech-ECE-Notes-2022-24',
+        },
+        {
+            name: 'Drive',
+            link: 'https://drive.google.com/drive/u/7/folders/17bng9aIkZ3FaULebbgEGpdCsB225dr_K',
+        },
+    ];
+
+    const handleNavigate = ({ link = null, nav = null }) => {
+        if (link) {
+            window.open(link, '_blank');
+        } else {
+            navigate(nav);
+        }
+    }
+
     return (
-        <Main>
+        <Main component='footer'>
             <Typography variant='body2'>
                 All Rights Reserved.
                 {" "}
@@ -38,6 +76,19 @@ const Footer = () => {
                     >Kunal Keshan</Link>
                 }.
             </Typography>
+            <NavLinks>
+                {links.map((link, index) => (
+                    <Link
+                        key={index}
+                        component='a'
+                        onClick={() => handleNavigate(link)}
+                        sx={{
+                            cursor: 'pointer',
+                            fontSize: '0.8rem',
+                        }}
+                    >{link.name}</Link>
+                ))}
+            </NavLinks>
         </Main>
     )
 }
@@ -45,6 +96,15 @@ const Footer = () => {
 const Main = styled(Box)({
     marginTop: 'auto',
     padding: '.5em',
-})
+});
+
+const NavLinks = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '1em',
+    flexWrap: 'wrap',
+    margin: '0.5em',
+});
 
 export default Footer;
