ac2b4a77b28d77c9f76577d24af90f62ea97c1e7
Kunal Keshan <kunalkeshan12@gmail.com>
2022-07-31 14:17:32 +0530

    ga start console

--- src/hooks/useGA.js
+++ src/hooks/useGA.js
@@ -11,6 +11,7 @@ const useGA = () => {
     useEffect(() => {
         if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
             ReactGA.initialize(config.GA_TRACKING_ID);
+            console.log('Analytics Initialized.')
         }
     }, []);
 };
