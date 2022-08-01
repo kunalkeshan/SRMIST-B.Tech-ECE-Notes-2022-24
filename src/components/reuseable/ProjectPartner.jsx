a402eb399993c96d1a3b943f16cb6560373d4bd5
Kunal Keshan <kunalkeshan12@gmail.com>
2022-08-01 13:35:02 +0530

    adding project partners components

---
+++ src/components/reuseable/ProjectPartner.jsx
@@ -0,0 +1,56 @@
+/**
+ * Project Partner Component
+ */
+
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+import config from '../../config';
+
+// MUI
+import { Typography, Paper, styled, Avatar, Tooltip } from '@mui/material';
+
+const ProjectPartner = ({ name, url, logo, scale }) => {
+
+    const handleNavigate = () => {
+        window.open(url, '_blank');
+    };
+
+    return (
+        <Tooltip title={`Learn more about ${name}!`}>
+            <Container
+                onClick={handleNavigate}
+                sx={{ transform: `scale(${scale})` }}
+            >
+                <Avatar src={logo} alt={name} />
+                <Typography>{name}</Typography>
+            </Container>
+        </Tooltip>
+    )
+}
+
+const Container = styled(Paper)({
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0.25em 0.5em',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    transition: 'all 250ms ease-in-out',
+    width: 'clamp(100px, 100%, 200px)',
+    '&:hover': {
+        backgroundColor: config.APP_COLORS.accent,
+    }
+});
+
+export default ProjectPartner;
+
+ProjectPartner.propTypes = {
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    logo: PropTypes.string.isRequired,
+    scale: PropTypes.number.isRequired,
+};
+
+ProjectPartner.defaultProps = {
+    scale: 1,
+}
--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,9 @@
  * Application Config
  */
 
+// Dependencies
+import CodelanceDevsLogo from '../assets/codelance-devs-logo.png';
+
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
@@ -12,6 +15,14 @@ const APP_COLORS = {
     darkAccent: '#465362',
 };
 
+const APP_PARTNERS = [
+    {
+        name: 'Codelance Devs',
+        url: 'https://www.linkedin.com/company/codelance-devs/',
+        logo: CodelanceDevsLogo,
+    }
+];
+
 const config = {
     WEBSITE_URL: `${window.location.protocol}//${window.location.host}`,
     GITHUB_URL: 'https://github.com/kunalkeshan/SRMIST-B.Tech-ECE-Notes-2022-24',
@@ -19,6 +30,7 @@ const config = {
     GA_TRACKING_ID: 'G-4FNHZ2Y1KD',
     FORMSPREEE_CONTACT_FORM_ID: 'xoqrgzkz',
     APP_COLORS,
+    APP_PARTNERS,
     CONTACT: {
         email: 'kunalkeshan12@gmail.com',
         instagram: 'https://www.instagram.com/kunalkeshan/',
