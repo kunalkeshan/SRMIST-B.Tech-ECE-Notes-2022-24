4d11bd65ec068dbf0859b43c247265182fe5516d
Kunal Keshan <kunalkeshan12@gmail.com>
2022-09-07 13:26:44 +0530

    extra notes page setup

--- src/pages/ExtraNotes.jsx
+++ src/pages/ExtraNotes.jsx
@@ -3,24 +3,50 @@
  */
 
 // Dependencies
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Data from '../utils/data';
 
 // MUI
-import { Box, styled, Typography, Divider, TextField, Button, Paper, Tooltip, Link, List, ListItem } from '@mui/material';
+import { Box, styled, Typography, Divider, Button, Tooltip, Link, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
 
 
 const ExtraNotes = () => {
+    const [extraNotes, setExtraNotes] = useState([]);
 
-    const Item = ({name, description, profile, link, title, dateAdded}) => {
-        return <ListItem></ListItem>
+    useEffect(() => {
+        const handleGetExtraNotes = async () => {
+            const data = await Data.ExtraNotes.getAllExtraNotes();
+            setExtraNotes(data);
+        }
+        if (extraNotes.length === 0) handleGetExtraNotes();
+    }, [])
+
+    const Item = ({ name, description, profile, link, title, dateAdded }) => {
+        return <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Tooltip title={`Visit ${name}'s Profile`}>
+                    <Link href={profile} target='_blank' underline='none'>
+                        <Avatar alt={name} src={profile} />
+                    </Link>
+                </Tooltip>
+            </ListItemAvatar>
+            <ListItemText
+                primary={title}
+                secondary={`Related to: ${description.join(', ')}`}
+            />
+        </ListItem>
     }
 
     return (
         <Main>
             <Typography variant='h1' fontSize='4rem'>Extra Notes</Typography>
             <Divider />
-            <Typography variant='h4' fontSize='2rem'>Collection of notes that don't directly relate to the project, but are helpful for those from different domains and fields!</Typography>
-            <ListContainer></ListContainer>
+            <Typography variant='h4' fontSize='1.4rem'>Collection of notes that don't directly relate to the project, but are helpful for those from different domains and fields!</Typography>
+            <ListContainer>
+                {extraNotes.length > 0 && extraNotes.map((extra, index) => (
+                    <Item key={index} {...extra} />
+                ))}
+            </ListContainer>
         </Main>
     )
 }
