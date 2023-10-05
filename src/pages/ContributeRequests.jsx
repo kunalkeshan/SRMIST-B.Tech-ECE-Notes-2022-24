8bc04aa7348489e3e9b4ceff2ec163d0b15d7ab2
SARVESH DEVRUKHAKAR <79737447+SAD0XER@users.noreply.github.com>
2023-10-05 15:57:21 +0530

    Client - Update Button to Anchor Element in Contribute Requests Page #122

--- src/pages/ContributeRequests.jsx
+++ src/pages/ContributeRequests.jsx
@@ -6,126 +6,149 @@
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { format } from 'date-fns';
-import config from '../config';
-
-// MUI
-import { Typography, Box, styled, Divider, Grid, Paper, Stack, Chip, Tooltip, Button } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 // Custom
+import config from '../config';
 import { fetchProjectIssuesList } from '../utils/axios';
 
+// MUI
+import {
+  Typography,
+  Box,
+  styled,
+  Divider,
+  Grid,
+  Paper,
+  Stack,
+  Chip,
+  Tooltip,
+  Button,
+} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 const ContributeRequests = () => {
-    const [issues, setIssues] = useState([]);
+  const [issues, setIssues] = useState([]);
 
-    useEffect(() => {
-        const handleFetchIssues = async () => {
-            const data = await fetchProjectIssuesList();
-            setIssues(data);
-        }
-        handleFetchIssues();
-    }, []);
+  useEffect(() => {
+    const handleFetchIssues = async () => {
+      const data = await fetchProjectIssuesList();
+      setIssues(data);
+    };
+    handleFetchIssues();
+  }, []);
 
-    const GitHubIssueCard = ({ title, body, state, updatedAt, createdAt, labels, url }) => {
+  const GitHubIssueCard = ({ title, body, state, updatedAt, createdAt, labels, url }) => {
 
-        const handleNavigateToIssue = () => {
-            window.open(url, '_blank');
-        }
-
-        return (
-            <Grid item xs={12} md={4} m={2}>
-                <Card >
-                    <Typography variant='h6'><GitHubIcon /> {title} - <Chip label={state} variant='outlined' color='success' sx={{ transform: 'scale(0.8)' }} /></Typography>
-                    <Divider />
-                    <Typography p={2} sx={{
-                        'img': {
-                            width: '100%',
-                        }
-                    }}>
-                        <ReactMarkdown>{body}</ReactMarkdown>
-                    </Typography>
-                    <GitHubButton
-                        startIcon={<GitHubIcon />}
-                        onClick={handleNavigateToIssue}
-                    >View Issue in GitHub</GitHubButton>
-                    <Divider />
-                    {labels.length > 0 && labels.map((label) => (
-                        <Tooltip
-                            title={label.description}
-                        >
-                            <Chip
-                                label={label.name}
-                                sx={{
-                                    cursor: 'pointer',
-                                    backgroundColor: `#${label.color}`,
-                                    margin: '0.5em 0.25em',
-                                }}
-                            />
-                        </Tooltip>
-                    ))}
-                    <Divider />
-                    <Stack direction='row' spacing={2} mt={1}>
-                        <p><b>Created At</b>: {format(new Date(createdAt), 'PPP')}</p>
-                        <p><b>Updated At</b>: {format(new Date(updatedAt), 'PPP')}</p>
-                    </Stack>
-                </Card>
-            </Grid>
-        )
-    }
+    const handleNavigateToIssue = () => {
+      window.open(url, '_blank');
+    };
 
     return (
-        <Main>
-            <Typography variant='h1' fontSize='4rem'>Contribution Requests</Typography>
-            <Divider />
-            {issues.length > 0 ?
-                <CardsContainer
-                    container
-                    spacing={2}
-                    gap={2}
-                    justifyContent='center'
-                >
-                    {
-                        issues.map((issue, index) => (
-                            <GitHubIssueCard {...issue} key={index} />
-                        ))
-                    }
-                </CardsContainer> :
-                <Typography>No issues available right now! 🥳🙌</Typography>
-            }
-        </Main >
-    )
-}
+      <Grid item xs={12} md={4} m={2}>
+        <Card>
+          <Typography variant='h6'>
+            <GitHubIcon /> {title} -{' '}
+            <Chip label={state} variant='outlined' color='success' sx={{ transform: 'scale(0.8)' }} />
+          </Typography>
+          <Divider />
+          <Typography
+            p={2}
+            sx={{
+              'img': {
+                width: '100%',
+              },
+            }}
+          >
+            <ReactMarkdown>{body}</ReactMarkdown>
+          </Typography>
+          {/* Replaced GitHubButton with an anchor tag */}
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              backgroundColor: config.APP_COLORS.accent,
+              color: config.APP_COLORS.darkAccent,
+              width: '100%',
+              marginBottom: '1em',
+              display: 'inline-block',
+              textAlign: 'center',
+              padding: '12px 24px',
+              textDecoration: 'none',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              '&:hover': {
+                backgroundColor: config.APP_COLORS.accent,
+                color: config.APP_COLORS.dark,
+              },
+            }}
+          >
+            <GitHubIcon /> View Issue in GitHub
+          </a>
+          <Divider />
+          {labels.length > 0 && labels.map((label) => (
+            <Tooltip title={label.description} key={label.name}>
+              <Chip
+                label={label.name}
+                sx={{
+                  cursor: 'pointer',
+                  backgroundColor: `#${label.color}`,
+                  margin: '0.5em 0.25em',
+                }}
+              />
+            </Tooltip>
+          ))}
+          <Divider />
+          <Stack direction='row' spacing={2} mt={1}>
+            <p><b>Created At</b>: {format(new Date(createdAt), 'PPP')}</p>
+            <p><b>Updated At</b>: {format(new Date(updatedAt), 'PPP')}</p>
+          </Stack>
+        </Card>
+      </Grid>
+    );
+  };
+
+  return (
+    <Main>
+      <Typography variant='h1' fontSize='4rem'>Contribution Requests</Typography>
+      <Divider />
+      {issues.length > 0 ? (
+        <CardsContainer
+          container
+          spacing={2}
+          gap={2}
+          justifyContent='center'
+        >
+          {issues.map((issue, index) => (
+            <GitHubIssueCard {...issue} key={index} />
+          ))}
+        </CardsContainer>
+      ) : (
+        <Typography>No issues available right now! 🥳🙌</Typography>
+      )}
+    </Main>
+  );
+};
 
 const Main = styled(Box)({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  '> *': {
     width: '100%',
-    '> *': {
-        width: '100%',
-    }
+  },
 });
 
 const CardsContainer = styled(Grid)({
-    marginTop: '1em',
-    width: '100%',
+  marginTop: '1em',
+  width: '100%',
 });
 
 const Card = styled(Paper)({
-    padding: '1em 0.75em',
-    height: '100%'
+  padding: '1em 0.75em',
+  height: '100%',
 });
-
-const GitHubButton = styled(Button)({
-    backgroundColor: config.APP_COLORS.accent,
-    color: config.APP_COLORS.darkAccent,
-    width: '100%',
-    marginBottom: '1em',
-    '&:hover': {
-        backgroundColor: config.APP_COLORS.accent,
-        color: config.APP_COLORS.dark,
-    }
-})
 
 export default ContributeRequests;
